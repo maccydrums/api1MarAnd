8627b96a7d140ddc92884f318d17a3354e5a07b0
maccydrums <marcusprivat@live.com>
2019-05-25 12:56:22 +0200

    added Vue

---
+++ show-api/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Nunito');
+#app {
+  font-family: 'Nunito', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+#app img {
+  width: 18%;
+}
+</style>
---
+++ show-api/src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h2>Students</h2>
+    <ul v-for="user in users"
+    :key="user.student.name"
+    >
+      <li>{{ user.student.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+      return {
+        users: []
+      }
+  },
+  methods: {
+    fetchData: function () {
+      const myRequest = new Request('http://localhost:3000/students')
+
+      fetch(myRequest)
+        .then((response) => { return response.json() })
+        .then((data) => {
+          this.users = data
+        }).catch( error => { console.log(error); });
+    }
+  },
+
+  mounted() {
+    this.fetchData()
+    console.log(this.users)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
